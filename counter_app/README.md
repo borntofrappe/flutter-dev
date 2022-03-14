8ef279fe96995537aea4ffd581d8ab3913afbdb9
borntofrappe <borntofrappe@gmail.com>
2022-03-14 16:12:38 +0100

    designed counter app

--- counter_app/README.md
+++ counter_app/README.md
@@ -2,8 +2,6 @@
 
 [flutter.dev/learn](https://flutter.dev/learn) introduces the development kit with a series of images, among which you find a prompt to create a flutter application and create a counter application.
 
-![Counter app](https://storage.googleapis.com/cms-storage-bucket/740d82517a6f13db51bd.png)
-
 The goal of this project is to immediately recreate the layout, but then try and develop a more elaborate application in which the numbers scroll vertically.
 
 ## Counter
@@ -135,3 +133,23 @@ Store the new value.
 ```dart
 instance.setInt('counter', _counter);
 ```
+
+## Design
+
+The goal is to mirror the design provided on flutter.dev/learn.
+
+![Counter app](https://storage.googleapis.com/cms-storage-bucket/740d82517a6f13db51bd.png)
+
+### Layout
+
+Describe the widget tree.
+
+### Style
+
+Describe:
+
+- color
+
+- gradient
+
+- rounded borders
--- counter_app/lib/counter.dart
+++ counter_app/lib/counter.dart
@@ -33,29 +33,83 @@ class _CounterState extends State<Counter> {
     _getCounter();
   }
 
+  Color color = const Color(0xff043875);
+
+  LinearGradient linearGradient = const LinearGradient(
+      colors: [Color(0xffc6bafa), Color(0xffefbad7)],
+      stops: [0.7, 1],
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Counter',
         home: Scaffold(
-            body: SafeArea(
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-              Text('$_counter'),
-              Row(
+            body: Container(
+          decoration: BoxDecoration(gradient: linearGradient),
+          child: SafeArea(
+              child: Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  IconButton(
-                      icon: const Icon(
-                        Icons.remove,
-                        semanticLabel: 'Decrement',
-                      ),
-                      onPressed: () => _updateCounter(-1)),
-                  IconButton(
-                      icon: const Icon(Icons.add, semanticLabel: 'Increment'),
-                      onPressed: () => _updateCounter(1))
-                ],
-              )
-            ]))));
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 32.0),
+                    child: Text('Counter',
+                        style: TextStyle(
+                            color: color,
+                            fontSize: 32.0,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Ubuntu')),
+                  ),
+                  Expanded(
+                    child: Center(
+                        child: Text('$_counter',
+                            style: TextStyle(
+                                color: color,
+                                fontSize: 256.0,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'Ubuntu'))),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 32.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(8.0),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: color, width: 4.0),
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                          child: IconButton(
+                              iconSize: 52.0,
+                              icon: Icon(
+                                Icons.remove,
+                                color: color,
+                                semanticLabel: 'Decrement',
+                              ),
+                              onPressed: () => _updateCounter(-1)),
+                        ),
+                        Container(
+                            padding: const EdgeInsets.all(8.0),
+                            decoration: BoxDecoration(
+                              border: Border.all(color: color, width: 4.0),
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                            child: IconButton(
+                                iconSize: 52.0,
+                                icon: Icon(Icons.add,
+                                    color: color, semanticLabel: 'Increment'),
+                                onPressed: () => _updateCounter(1)))
+                      ],
+                    ),
+                  )
+                ]),
+          )),
+        )));
   }
 }
