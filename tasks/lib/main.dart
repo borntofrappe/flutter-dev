d3b4efc1d519e354181a8907cd124217b7dfd949
borntofrappe <borntofrappe@gmail.com>
2022-03-25 11:44:33 +0100

    conditional floating action button
    add floating action button; prompt a dialog with an autofocus text field; condition the floating action button to the text field having focus

--- tasks/lib/main.dart
+++ tasks/lib/main.dart
@@ -29,15 +29,57 @@ class MyTasksPage extends StatefulWidget {
 }
 
 class _MyTasksPageState extends State<MyTasksPage> {
+  late FocusNode _focusNode;
+  bool _hasFocus = false;
+
+   @override
+  void initState() {
+    super.initState();
+
+    _focusNode = FocusNode();
+    _focusNode.addListener(() {
+      setState(() {
+        _hasFocus = _focusNode.hasFocus;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
-      body: const Align(
-        alignment: Alignment.bottomCenter,
-        child: TextInput(),
+      floatingActionButton: _focusNode.hasFocus ? null : FloatingActionButton(
+        child: const Icon(
+          Icons.add, 
+          color: Colors.white,
+        ),
+        onPressed: () {
+          setState(() {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) => Dialog(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    onSubmitted: (String text) {
+                      Navigator.pop(context);
+                    },
+                    focusNode: _focusNode,
+                    autofocus: true,
+                  ),
+                )
+              )
+            );
+          });
+        }
       ),
-      floatingActionButton: null,
     );
   }
 }
