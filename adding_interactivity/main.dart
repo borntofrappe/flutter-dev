e0357fa4410ce6fc72c462736909a4054a009c22
borntofrappe <borntofrappe@gmail.com>
2022-03-28 17:53:43 +0200

    adding_interactivity complete codelab

--- adding_interactivity/main.dart
+++ adding_interactivity/main.dart
@@ -47,8 +47,7 @@ class App extends StatelessWidget {
                   style: TextStyle(color: Colors.grey[500]))
             ]),
           ),
-          Icon(Icons.star, color: Colors.red[500]),
-          const Text('41')
+          FavoriteWidget(),
         ],
       ),
     );
@@ -97,6 +96,60 @@ class App extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class FavoriteWidget extends StatefulWidget {
+  const FavoriteWidget({Key? key}) : super(key: key);
+
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            padding: const EdgeInsets.all(0),
+            alignment: Alignment.center,
+            icon: _isFavorited
+                ? const Icon(
+                    Icons.star,
+                  )
+                : const Icon(
+                    Icons.star_border,
+                  ),
+            onPressed: _toggleFavorite,
+            color: Colors.red[500],
+          ),
+        ),
+        SizedBox(
+          width: 18.0,
+          child: Text(
+            '$_favoriteCount',
+          ),
+        ),
+      ],
     );
   }
 }
